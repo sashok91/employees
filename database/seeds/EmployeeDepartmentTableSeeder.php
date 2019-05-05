f62e9fe72a3c2555328ff9ede086b181efa0a1bd
sashok91 <al.shyrko@gmail.com>
2019-05-05 22:03:48 +0300

    updated seeds

--- database/seeds/EmployeeDepartmentTableSeeder.php
+++ database/seeds/EmployeeDepartmentTableSeeder.php
@@ -52,6 +52,86 @@ class EmployeeDepartmentTableSeeder extends Seeder
                 'id' => 8,
                 'employee_id' => 5,
                 'department_id' => 1
+            ],
+            [
+                'id' => 9,
+                'employee_id' => 6,
+                'department_id' => 1
+            ],
+            [
+                'id' => 10,
+                'employee_id' => 7,
+                'department_id' => 3
+            ],
+            [
+                'id' => 11,
+                'employee_id' => 7,
+                'department_id' => 1
+            ],
+            [
+                'id' => 12,
+                'employee_id' => 7,
+                'department_id' => 2
+            ],
+            [
+                'id' => 13,
+                'employee_id' => 8,
+                'department_id' => 3
+            ],
+            [
+                'id' => 14,
+                'employee_id' => 8,
+                'department_id' => 2
+            ],
+            [
+                'id' => 15,
+                'employee_id' => 9,
+                'department_id' => 2
+            ],
+            [
+                'id' => 16,
+                'employee_id' => 9,
+                'department_id' => 1
+            ],
+            [
+                'id' => 17,
+                'employee_id' => 10,
+                'department_id' => 1
+            ],
+            [
+                'id' => 18,
+                'employee_id' => 10,
+                'department_id' => 3
+            ],
+            [
+                'id' => 19,
+                'employee_id' => 11,
+                'department_id' => 1
+            ],
+            [
+                'id' => 20,
+                'employee_id' => 11,
+                'department_id' => 2
+            ],
+            [
+                'id' => 21,
+                'employee_id' => 12,
+                'department_id' => 3
+            ],
+            [
+                'id' => 22,
+                'employee_id' => 12,
+                'department_id' => 2
+            ],
+            [
+                'id' => 23,
+                'employee_id' => 13,
+                'department_id' => 2
+            ],
+            [
+                'id' => 24,
+                'employee_id' => 13,
+                'department_id' => 1
             ]
         ]);
     }
